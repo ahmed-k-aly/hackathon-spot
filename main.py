a4c5f5e28739a533fb2e9a9b03b262fa2ad4d956
ahmed <aaly24@amherst.edu>
2024-02-18 01:09:32 -0500

    file problems

--- main.py
+++ main.py
@@ -171,14 +171,13 @@ def say_something(text):
 def listen_to_microphone():
     say_something("Listening to the environment")
     
-    os.system("arecord -D plughw:1,0 -f cd -c1 -r 48000 -d 5 -t wav -V mono -v file.wav")
-    os.system("ffmpeg -i file.wav -acodec pcm_s16le -ac 1 -ar 16000 file.wav")
+    os.system("arecord -D plughw:1,0 -f cd -c1 -r 48000 -d 5 -t wav -V mono -v listen.wav")
+    os.system("ffmpeg -i file.wav -acodec pcm_s16le -ac 1 -ar 16000 listen.wav")
     
 
-    direction = estimate_audio_spatial_location("file.wav")
+    direction = estimate_audio_spatial_location("listen.wav")
     print(f"Sound is coming from the {direction}")
     
-    os.system("rm file.wav")
 
 
 
